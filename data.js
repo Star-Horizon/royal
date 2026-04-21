03a2a5da38c0f5e93e03ea16a19875d6e8412724
Heppoko-SH <heppoko.5731@gmail.com>
2026-04-22 00:23:58 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const CAST_LIST = [
+    { 
+        id: "remon",
+        name: "REMON", 
+        time: "20:00-LAST", 
+        imgs: ["./yuzu1.jpg", "./ema_2.jpg"],
+        bio: "CLUB ROYALのリーダー！お酒と楽しいお喋りが大好きです。TikTokライブも毎日配信中！" 
+    },
+    { 
+        id: "manoa",
+        name: "MANOA", 
+        time: "21:00-LAST", 
+        imgs: ["./mano_1.jpg", "./mano_2.jpg"],
+        bio: "癒やし系担当。落ち着いてゆっくり飲みたい方はぜひ会いに来てくださいね。"
+    }
+    // ...人数分追加
+];
